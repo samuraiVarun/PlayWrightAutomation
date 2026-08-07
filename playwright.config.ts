22686b1fd2f5152bbb12bbe52db5cdadf084e9ad
Varun <varun.samurai@gmail.com>
2026-08-07 12:26:08 +0530

    Removed errors from Config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,17 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
 
-  reporter: 'html',
-
-  use: {
-
-    screenshot: 'only-on-failure',
-
-    video: 'retain-on-failure',
-
-    trace: 'retain-on-failure'
-
-  },
+  
 
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -38,11 +28,20 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    
+
+    screenshot: 'only-on-failure',
+
+    video: 'retain-on-failure',
+
+    trace: 'retain-on-failure'
+
+  
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+ 
   },
 
   /* Configure projects for major browsers */
